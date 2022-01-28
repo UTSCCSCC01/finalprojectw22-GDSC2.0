c48a582923eade2b61a0448c734e59712efb6658
mandalaw <al.mandalawi@mail.utoronto.ca>
2022-01-28 16:40:01 -0500

    process.md commit
    process.md submission

---
+++ backend/server.js
@@ -0,0 +1,39 @@
+// required frameworks/modules
+require("dotenv").config();
+const express = require("express");
+const connDB = require("./config/db");
+const testModel = require("./models/testModel");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use(bodyParser.json());
+
+// connect to database
+connDB();
+
+app.get("/", (req, res) => {
+  res.send("API IS RUNNING...");
+});
+
+app.get("/data", (req, res) => {
+  testModel
+    .find({})
+    .then((data) => res.json(data))
+    .catch((e) => console.log(e));
+});
+
+app.post("/", (req, res) => {
+  console.log(req.body.test);
+  testModel.create({ name: req.body.test }).then((id) => {
+    console.log(`inserted: ${id}`);
+  });
+  res.send("inserted");
+});
+
+app.listen(
+  process.env.PORT,
+  console.log(`listening on port ${process.env.PORT}`)
+);
---
+++ frontend/src/App.js
@@ -0,0 +1,51 @@
+import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const App = () => {
+  const [test, setTest] = useState("");
+  const [input, setInput] = useState("");
+
+  const submit = async () => {
+    await axios.post("/", { test: input });
+    // console.log(a);
+
+    getData();
+  };
+
+  const getData = async () => {
+    const db = await axios.get("/data");
+    console.log(db.data);
+    let display = "";
+    for (var i = 0; i < db.data.length; i++) {
+      display += db.data[i].name + ", ";
+    }
+    console.log(display);
+    setTest(display);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return (
+    <div className="App">
+      <input
+        onChange={(event) => setInput(event.target.value)}
+        type="text"
+        id="test"
+        name="test"
+      />
+      <button onClick={submit} type="submit">
+        click me to add to test model in DB
+      </button>
+
+      <div>
+        <b>Test Database Data: </b>
+        {test}
+      </div>
+    </div>
+  );
+};
+
+export default App;
