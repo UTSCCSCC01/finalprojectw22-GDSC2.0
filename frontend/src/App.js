6982fe9ac955f6c985fa7adc456ba54202c12c85
stephanmotha <stephan.motha@outlook.com>
2022-02-04 20:57:13 -0500

    [GD-27] add template forms init

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,20 +4,20 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 // import Navbar from "./components/Navbar";
 import axios from "axios";
 import Initial from "./components/application/Initial/Initial";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import StudentForm from "./components/application/forms/StudentForm";
 
 const App = () => {
   return (
     <>
-    {/* <div>Hello</div> */}
-      {/* <Navbar /> */}
       <Router>
         <Routes>
           <Route path="/" element />
           <Route path="/projects" element />
           <Route path="/resources" element />
           <Route path="/events" element />
-          <Route path="/portal" element={<Initial/>} />
+          <Route path="/portal" element={<Initial />} />
+          <Route path="/portal/form" element={<StudentForm />} />
           <Route path="/team" element />
           <Route path="/subscribe" element />
         </Routes>
--- frontend/src/components/application/Initial/Initial.js
+++ frontend/src/components/application/Initial/Initial.js
@@ -6,17 +6,29 @@ import Button from "react-bootstrap/Button";
 
 const Initial = () => {
   return (
-    <div class="container">
-      <h1>Welcome [Name of User]...</h1>
-      <div class="sub">
-        <Button size="lg" variant="outline-primary">
-          Student Application
-        </Button>
-        <Button size="lg" variant="outline-danger">
-          Mentor Application
-        </Button>
+    <>
+      <style type="text/css">
+        {`
+    .btn-large {
+      padding: 10px 20px 10px 20px;
+      font-size: 2.5vw;
+    }
+    
+    `}
+      </style>
+      <div class="container">
+        <h1>Welcome [Name of User]...</h1>
+        <div class="sub">
+          <Button size="large" variant="outline-success">
+            Student Application
+          </Button>
+          <div class="vl"></div>
+          <Button size="large" variant="outline-danger">
+            Mentor Application
+          </Button>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
