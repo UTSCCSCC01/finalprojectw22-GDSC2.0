8e28ba4cf2f78b3ed82d4482880cb6cf93dd7fea
Sahib Nanda <70871918+sahibnanda@users.noreply.github.com>
2022-02-07 18:23:52 -0500

    [GD-1] Adding new Navbar and Images folders, and updating starter files (#22)
    * Adding new Navbar and Images folders, and updating starter files required by Navbar
    
    * [GD-1] merge files
    
    Co-authored-by: Sahib <sahib.nanda@mail.utoronto.ca>
    Co-authored-by: stephanmotha <stephan.motha@outlook.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,15 @@
 import "./App.css";
 import { useEffect, useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-// import Navbar from "./components/Navbar";
+
 import axios from "axios";
+import Navbarmenu from "./components/Navbarmenu/Navbarmenu";
+import Homepage from "./components/Homepage/Homepage"
+import About from "./components/About/About"
+import Subscribe from "./components/Subscribe/Subscribe"
+import ContactUs from "./components/ContactUs/ContactUs"
+import Team from "./components/Team/Team"
+
 import Initial from "./components/application/Initial/Initial";
 import "bootstrap/dist/css/bootstrap.min.css";
 import StudentForm from "./components/application/forms/StudentForm";
@@ -11,21 +18,28 @@ import PageNotFound from "./components/PageNotFound/PageNotFound";
 
 const App = () => {
   return (
+    <>
+    <Navbarmenu />
     <Router>
       <Routes>
-        <Route path="/" element />
-        <Route path="projects" element />
-        <Route path="resources" element />
-        <Route path="events" element />
+        <Route path="/" element={ <Homepage/> } />
+        <Route path="/projects" element />
+        <Route path="/resources" element />
+        <Route path="/events" element />
+        <Route path="/team" element={ <Team />} />
+        <Route path="/subscribe" element={ <Subscribe/>} />
+        <Route path="/about" element={ <About/>} />
+        <Route path="/contactUs" element={ <ContactUs/>} />
 
-        <Route path="team" element />
-        <Route path="subscribe" element />
         <Route path="portal/:id" element={<Initial />} />
         <Route path="portal/:id/student/app/" element={<StudentForm />} />
         <Route path="portal/:id/mentor/app/" element={<MentorForm />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </Router>
+
+    </>
+
   );
 };
 
---
+++ frontend/src/components/Navbarmenu/Navbarmenu.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import dsclogo from "../../images/dsclogo.png"
+import { Nav, Navbar, Container } from "react-bootstrap"
+
+const Navbarmenu = () => {
+  return (
+      <Navbar collapseOnSelect expand="lg" >
+        <Container>
+          <Nav.Link href="/">
+            <img src={ dsclogo } alt="dsc_logo" height="30px" />
+          </Nav.Link>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="/about">About</Nav.Link>
+              <Nav.Link href="/applications">Applications</Nav.Link>
+              <Nav.Link href="/team">Team</Nav.Link>
+              <Nav.Link href="/subscribe">Subscribe</Nav.Link>
+              <Nav.Link href="/contactUs">Contact Us</Nav.Link>
+            </Nav>
+            <Nav>
+              <Nav.Link href="/myProfile">My Profile</Nav.Link>
+              <Nav.Link href="/signOut">Sign Out</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+  )
+};
+
+export default Navbarmenu;
