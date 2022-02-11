bb0ddb7e2ec1e7057a363731f984d87a53b9524e
mandalaw <al.mandalawi@mail.utoronto.ca>
2022-02-10 21:45:30 -0500

    Register initial push
    Created Register an Account feature for login page (student/mentor). Needs further backend connections.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import axios from "axios";
 import Login from "./components/Login"
+import Register from "./components/Register/Register"
 import Navbarmenu from "./components/Navbarmenu/Navbarmenu";
 import Homepage from "./components/Homepage/Homepage";
 import About from "./components/About/About";
@@ -13,6 +14,8 @@ import Initial from "./components/application/Initial/Initial";
 import "bootstrap/dist/css/bootstrap.min.css";
 import StudentForm from "./components/application/forms/StudentForm";
 import MentorForm from "./components/application/forms/MentorForm";
+import AdminPage from "./components/admin/AdminPage";
+import AdminLogin from "./components/admin/AdminLogin";
 import PageNotFound from "./components/PageNotFound/PageNotFound";
 
 const App = () => {
@@ -30,9 +33,12 @@ const App = () => {
           <Route path="/about" element={<About />} />
           <Route path="/contactUs" element={<ContactUs />} />
           <Route path="/portal" element={<Login />} />
+          <Route path="/register" element={<Register />} />
           <Route path="portal/:id" element={<Initial />} />
           <Route path="portal/:id/student/app/" element={<StudentForm />} />
           <Route path="portal/:id/mentor/app/" element={<MentorForm />} />
+          <Route path="/admin/log" element={<AdminLogin />} />
+          <Route path="/admin/main" element={<AdminPage />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </Router>
