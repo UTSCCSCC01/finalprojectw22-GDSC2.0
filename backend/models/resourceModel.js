b16dc606ed48d5aae37d6e4981e19024be7a10d5
Stephan Motha <stephan.motha@outlook.com>
2022-03-03 19:59:26 -0500

    [GD-17] resources view (#38)
    * [GD-37] initialized Admin resource view and logic
    
    * [GD-37] update add and delete resource functionality
    
    * [GD-37] input validation and updated formatting
    
    * [GD-37] update button size
    
    * [GD-37] add resource model andd update form
    
    * [GD-37] update for labels
    
    * [GD-37] update column size

---
+++ backend/models/resourceModel.js
@@ -0,0 +1,27 @@
+// Each file will represent a different collection in database
+
+const mongoose = require("mongoose");
+const UserSchema = new mongoose.Schema({
+  // define field and values this schema should have.
+  section: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  link: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+});
+
+//takes the name of the collection and the schema that represents it
+const ResourceModel = mongoose.model("resources", UserSchema);
+
+module.exports = UserModel;
