03a1b1acbf8b345ecc50990781bdd9dd1113aa94
Litao Chen <55116264+JacobChen258@users.noreply.github.com>
2022-03-15 18:18:06 -0400

    [GD-45] complete admin project view

--- frontend/src/components/admin/AdminPage.js
+++ frontend/src/components/admin/AdminPage.js
@@ -7,6 +7,7 @@ import AdminStyle from "../../css/admin/AdminPage.module.css";
 import AdminApplication from "./AdminApplications"
 import {Button} from "react-bootstrap"
 import axios from "axios"
+import AdminPastProject from "./AdminPastProject";
 
 export default function AdminPage(props){
     // reference: https://gdscutm.com
@@ -94,7 +95,9 @@ export default function AdminPage(props){
                             <Card.Header>
                                 Past Projects
                             </Card.Header>
-                            <Card.Body></Card.Body>
+                            <Card.Body>
+                                <AdminPastProject/>
+                            </Card.Body>
                         </Card>
                     </Accordion.Collapse>
                 </Accordion>
