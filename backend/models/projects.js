1e423b9268e6fcb159bec2a01d6018d7998161e6
DaleRodrigues100 <77642080+DaleRodrigues100@users.noreply.github.com>
2022-03-16 19:25:04 -0400

    [GD-47] modifies database models (#53)
    * [GD-47] modifies database models
    
    * Update resourceModel.js
    
    * Delete teamMembers.js
    
    Duplicate files
    
    * [GD-47] Delete team.js
    
    Duplicated file

--- backend/models/projects.js
+++ backend/models/projects.js
@@ -1,16 +1,16 @@
 // Each file will represent a different collection in database
 
 const mongoose = require("mongoose");
-const resourceSchema = new mongoose.Schema({
+const projectSchema = new mongoose.Schema({
   // define field and values this schema should have.
   
   name: {
     type: String,
     required: true,
   },
-  image:{
-    type:String,
-    required: true,
+  ImagePath: {
+      type:String,
+      required: true,
   },
   link: {
     type: String,
@@ -23,6 +23,6 @@ const resourceSchema = new mongoose.Schema({
 });
 
 //takes the name of the collection and the schema that represents it
-const ResourceModel = mongoose.model("resources", resourceSchema );
+const ProjectModel = mongoose.model("resources", projectSchema);
 
-module.exports = ResourceModel;
+module.exports = ProjectModel;
--- backend/models/testModel.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-const testModel = mongoose.model("testModel", new Schema({ name: String }));
-
-module.exports = testModel;
