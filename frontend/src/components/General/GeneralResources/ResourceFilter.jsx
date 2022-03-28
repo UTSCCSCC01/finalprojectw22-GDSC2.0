10cb4f45c157f7c98a683b55235274f6700a833e
stephanmotha <stephan.motha@outlook.com>
2022-03-27 22:35:23 -0400

    [GD-19] add view to resources to match events

--- frontend/src/components/General/GeneralResources/ResourceFilter.jsx
+++ frontend/src/components/General/GeneralResources/ResourceFilter.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { Button, Row, Modal } from "react-bootstrap";
+import AppModule from "../../../css/admin/Application.module.css";
 
 const ResourceFilter = ({ resources }) => {
   const [filter, setFilter] = useState("");
@@ -8,7 +10,18 @@ const ResourceFilter = ({ resources }) => {
     setFilter(event.target.value);
   };
 
+  const initResourceInfo = {
+    id: "",
+    section: "",
+    name: "",
+    link: "",
+    description: "",
+  };
+
   const [resourcesState, setResourcesState] = useState([]);
+  const [resourceInfo, setResourceInfo] = useState(initResourceInfo);
+  // show modal when state is true
+  const [resourceModal, setResourceModal] = useState(false);
 
   let getResources = async () => {
     axios.get("/resources").then((res) => {
@@ -30,11 +43,17 @@ const ResourceFilter = ({ resources }) => {
     return sections;
   };
 
+  const handleViewResource = (resource) => {
+    console.log(resource);
+    setResourceInfo(resource);
+    setResourceModal(true);
+  };
+
   let dataSearch = (section) =>
     resourcesState.filter((item) => {
       return (
         Object.keys(item)
-          .filter((elem) => elem != "_id")
+          .filter((elem) => elem !== "_id")
           .some((key) => {
             console.log(Object.keys(item));
             return item[key]
@@ -46,52 +65,103 @@ const ResourceFilter = ({ resources }) => {
     });
 
   return (
-    <section className="py-4 container">
-      <div className="row justify-content-center">
-        <div className="col-12 mb-5">
-          <div className="mb-3 col-4 mx-auto text-center">
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Filter"
-              value={filter}
-              onChange={search.bind(this)}
-            />
+    <>
+      <section className="py-4 container">
+        <div className="row justify-content-center">
+          <div className="col-12 mb-5">
+            <div className="mb-3 col-4 mx-auto text-center">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Filter"
+                value={filter}
+                onChange={search.bind(this)}
+              />
+            </div>
           </div>
-        </div>
-        {[...getSections()].map((section) => {
-          console.log(resourcesState);
-          let data = dataSearch(section).map((item, index) => {
-            return (
-              <div key={index} className="col-11 col-md-6 col-lg-3 mx-0 mb-4">
-                <a
-                  href={item.link}
-                  style={{ textDecoration: "none", color: "black" }}
-                >
-                  <div className="card p-0 overflow-hidden h-100 shadow">
+          {[...getSections()].map((section) => {
+            console.log(resourcesState);
+            let data = dataSearch(section).map((item, index) => {
+              return (
+                <div key={index} className="col-11 col-md-6 col-lg-3 mx-0 mb-4">
+                  <div className="card p-0 overflow-hidden h-100 shadow text-center">
                     <div className="card-body">
-                      <h5 className="card-title">{item.name}</h5>
+                      <a
+                        href={item.link}
+                        style={{ textDecoration: "none", color: "black" }}
+                      >
+                        <h5 className="card-title">{item.name}</h5>
+                      </a>
                       <p className="card-text">{item.description}</p>
+                      <Button
+                        variant="secondary"
+                        onClick={() => handleViewResource(item)}
+                      >
+                        View
+                      </Button>
                     </div>
                   </div>
-                </a>
-              </div>
-            );
-          });
-          console.log(data.length);
+                </div>
+              );
+            });
+            console.log(data.length);
 
-          return (
-            data.length > 0 && (
-              <>
-                <h2>{section}</h2>
-                <hr />
-                {data}
-              </>
-            )
-          );
-        })}
-      </div>
-    </section>
+            return (
+              data.length > 0 && (
+                <>
+                  <h2>{section}</h2>
+                  <hr />
+                  {data}
+                </>
+              )
+            );
+          })}
+        </div>
+      </section>
+      <Modal
+        show={resourceModal}
+        onHide={() => setResourceModal(false)}
+        className="d-flex flex=column "
+        dialogClassName={`${AppModule.dialog_width}`}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Resource Information</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div className="d-flex w-100">
+            <h1 className="ms-2 mt-auto">{resourceInfo.section}</h1>
+            <h4 className={`ms-3 mt-auto mb-auto ${AppModule.role_text_color}`}>
+              {resourceInfo.name}
+            </h4>
+          </div>
+          <hr></hr>
+          <Row xs={1} md={1} className="g-4 ms-2">
+            <div>
+              <b>Link :</b> {resourceInfo.link}
+            </div>
+            <div>
+              <b>Description :</b> {resourceInfo.description}
+            </div>
+          </Row>
+          {/* <Row className="g-4 ms-2 mt-1">
+              <div>
+                <b>UofT email :</b> {studentInfo.ut_email}
+              </div>
+              <div>
+                <b>Profile Link :</b> {studentInfo.profile_link}
+              </div>
+            </Row> */}
+        </Modal.Body>
+        <Modal.Footer>
+          {/* <Button variant="primary me-auto" onClick={handleShowModal}>
+              Confirm
+            </Button>
+            <Button variant="secondary" onClick={handleShowModal}>
+              Dont Save
+            </Button> */}
+        </Modal.Footer>
+      </Modal>
+    </>
   );
 };
 
